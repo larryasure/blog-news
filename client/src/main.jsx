f21ebe1f7fb1585443248aaab57156e22ea4077e
Larryasure <olascolanre@gmail.com>
2026-06-06 16:29:01 +0100

    New Page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-`import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
@@ -85,4 +85,3 @@ createRoot(document.getElementById("root")).render(
     </AuthProvider>
   </StrictMode>,
 );
-`
